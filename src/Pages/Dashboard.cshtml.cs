5684f2037a1e7919c048e19e2662414cdafc7ff3
jack-nimbleapproach <128064239+jack-nimbleapproach@users.noreply.github.com>
2025-06-05 16:02:19 +0100

    Added landing page and fixed DSI (#13)

---
+++ src/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DfE.ExternalApplications.Web.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        private readonly ILogger<DashboardModel> _logger;
+        public string? Email { get; private set; }
+        public string? FirstName { get; private set; }
+        public string? LastName { get; private set; }
+        public string? OrganisationName { get; private set; }
+
+
+        public DashboardModel(ILogger<DashboardModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Email = User.FindFirst(ClaimTypes.Email)?.Value
+                    ?? User.FindFirst("email")?.Value;
+
+            FirstName = User.FindFirst(ClaimTypes.GivenName)?.Value;
+            LastName = User.FindFirst(ClaimTypes.Surname)?.Value;
+
+            var orgJson = User.FindFirst("organisation")?.Value;
+            if (!string.IsNullOrEmpty(orgJson))
+            {
+                try
+                {
+                    using var doc = JsonDocument.Parse(orgJson);
+                    OrganisationName = doc.RootElement
+                        .GetProperty("name")
+                        .GetString();
+                }
+                catch
+                {
+                    OrganisationName = null;
+                }
+            }
+
+        }
+    }
+    }
