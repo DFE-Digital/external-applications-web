4b5a95ff5c0e5acae8235e0511016c55bef2abed
Jack Marshall <jack.marshall@nimbleapproach.com>
2025-07-22 10:18:36 +0100

    Fixed apploication state not loading and minor changes to contributors page

--- src/DfE.ExternalApplications.Infrastructure/Services/ApplicationStateService.cs
+++ src/DfE.ExternalApplications.Infrastructure/Services/ApplicationStateService.cs
@@ -60,14 +60,10 @@ namespace DfE.ExternalApplications.Infrastructure.Services
 
                         logger.LogDebug("Using cached template schema for application {ApplicationId} with template version {TemplateVersionId}", 
                             sessionAppId, storedTemplateVersionId);
-                    }
-                    else
-                    {
-                        // For newly created applications, we don't have template schema
-                        logger.LogDebug("Using session-based application {ApplicationId} (no template schema available)", sessionAppId);
+
+                        return (sessionAppId, currentApplication);
                     }
 
-                    return (sessionAppId, currentApplication);
                 }
             }
 
@@ -108,8 +104,6 @@ namespace DfE.ExternalApplications.Infrastructure.Services
                     session.SetString(leadApplicantNameKey, application.CreatedBy!.Name);
                     session.SetString(leadApplicantEmailKey, application.CreatedBy.Email);
                     session.SetString(leadApplicantUserIdKey, application.CreatedBy.UserId.ToString());
-
-
 
                     // Load existing response data into session for existing applications
                     await LoadResponseDataIntoSessionAsync(application, session);
