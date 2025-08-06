0e7db7e3b91572ec5e735f1d66f1c0bbb114b342
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-08-06 14:45:25 +0100

    Fixed order of the academies and how the change link is duisplayed in the summary page

--- src/DfE.ExternalApplications.Web/Services/IAutocompleteService.cs
+++ src/DfE.ExternalApplications.Web/Services/IAutocompleteService.cs
@@ -3,6 +3,5 @@ namespace DfE.ExternalApplications.Web.Services
     public interface IAutocompleteService
     {
         Task<List<object>> SearchAsync(string complexFieldId, string query);
-       // Task<List<object>> SearchAsync(string endpoint, string query);
     }
 } 
