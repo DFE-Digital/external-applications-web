8b10c92ace8d4d34e1be3f5878d12c55ae70a1e4
FrostyApeOne <Farshad.DASHTI@EDUCATION.GOV.UK>
2025-10-14 10:24:17 +0100

    Updated cypress request header

--- src/DfE.ExternalApplications.Web/Services/ExternalAppsCypressRequestChecker.cs
+++ src/DfE.ExternalApplications.Web/Services/ExternalAppsCypressRequestChecker.cs
@@ -17,8 +17,8 @@ public class ExternalAppsCypressRequestChecker(
     ILogger<ExternalAppsCypressRequestChecker> logger)
     : ICustomRequestChecker
 {
-    private const string CypressHeaderKey = "X-Cypress-Test";
-    private const string CypressSecretHeaderKey = "X-Cypress-Secret";
+    private const string CypressHeaderKey = "x-cypress-test";
+    private const string CypressSecretHeaderKey = "x-cypress-secret";
     private const string ExpectedCypressValue = "true";
 
     /// <summary>
