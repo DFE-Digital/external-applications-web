2bdbbe5a4f005cbd17247148ecbb757c55580be9
Jack Marshall <jack.marshall@nimbleapproach.com>
2025-11-18 13:26:07 +0000

    Terms and Conditions and Privacy Notice pages with links in footer

---
+++ src/DfE.ExternalApplications.Web/Pages/Shared/PrivacyNotice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DfE.ExternalApplications.Web.Pages
+{
+    public class PrivacyNoticeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
