9e8e0a1c1518b22f80e1c670eb66d55993e75228
Elijah Aremu <45821029+elielijah321@users.noreply.github.com>
2026-05-27 10:55:39 +0100

    Feature/276384 pagination (#171)
    pagesize should default to 50
    
    
    ---------
    
    Co-authored-by: AREMU, Elijah <Elijah.AREMU@EDUCATION.GOV.UK>

--- src/DfE.ExternalApplications.Application/Options/DashboardOptions.cs
+++ src/DfE.ExternalApplications.Application/Options/DashboardOptions.cs
@@ -2,5 +2,5 @@ namespace DfE.ExternalApplications.Application.Options;
 
 public class DashboardOptions
 {
-    public int PageSize { get; set; } = 10;
+    public int PageSize { get; set; } = 50;
 }
