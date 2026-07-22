40b45f6f4769903bde3ccdf67e2deaaf345348cc
PRIOR <Stephen.PRIOR@EDUCATION.GOV.UK>
2026-07-22 15:52:48 +0100

    refactor csv generator to use field type exporters

--- src/DfE.ExternalApplications.Web/Services/ApplicationCsvGenerator.cs
+++ src/DfE.ExternalApplications.Web/Services/ApplicationCsvGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using DfE.ExternalApplications.Web.Interfaces;
+using GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Response;
 using HtmlAgilityPack;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 using System.Dynamic;
 using System.Text.Json;
@@ -9,13 +11,6 @@ namespace DfE.ExternalApplications.Web.Services
     public class ApplicationCsvGenerator : IApplicationCsvGenerator
     {
         private readonly JsonSerializerOptions serializerOptions = new() { WriteIndented = true };
-
-        private enum FieldType
-        {
-            String,
-            Object,
-            Array
-        }
 
         public Stream? Generate(string html)
         {
@@ -38,65 +33,23 @@ namespace DfE.ExternalApplications.Web.Services
             if (obj == null) return string.Empty;
 
             // flatten application response body into a CSV format
-            List<string> csvHeaders = [];
-            List<string> csvItems = [];
+            Csv csv = new();
 
             foreach (var kvp in obj)
             {
-                CsvWriter fieldWriter = new(csvHeaders, csvItems, kvp);
-                FieldType? fieldType = null;
                 if (kvp.Value is JsonElement jsonElement && jsonElement.ValueKind == JsonValueKind.Object)
                 {
-                    JsonElement.ObjectEnumerator nestedObjects = jsonElement.EnumerateObject();
-                    JsonElement value = nestedObjects.FirstOrDefault(x => x.Name == "value").Value;
-                    var valueString = value.ToString();
-                    if (valueString.StartsWith('[') && valueString.EndsWith(']'))
-                    {
-                        fieldType = FieldType.Array;
-                        // TODO parse specific objects - move to own class/method
-                        if (valueString.Contains("fileName"))
-                        {
-                            IEnumerable<JsonElement> objects = JsonSerializer.Deserialize<IEnumerable<JsonElement>>(valueString)!;
-                            Debug.WriteLine($"{objects.Count()} file upload objects");
-                            foreach (JsonElement obj2 in objects)
-                            {
-                                JsonElement.ObjectEnumerator nestedObjects2 = obj2.EnumerateObject();
-                                JsonElement originalFileName = nestedObjects2.FirstOrDefault(x => x.Name == "originalFileName").Value;
-                                JsonElement description = nestedObjects2.FirstOrDefault(x => x.Name == "description").Value;
-                                JsonElement fileSize = nestedObjects2.FirstOrDefault(x => x.Name == "fileSize").Value;
-                                JsonElement uploadedBy = nestedObjects2.FirstOrDefault(x => x.Name == "uploadedBy").Value;
-                                JsonElement uploadedByUser = nestedObjects2.FirstOrDefault(x => x.Name == "uploadedByUser").Value;
-                                JsonElement uploadedOn = nestedObjects2.FirstOrDefault(x => x.Name == "uploadedOn").Value;
-                                Debug.WriteLine($"{kvp.Key}.value - originalFileName: {originalFileName}, fileSize: {fileSize}.");
-                                fieldWriter.AddField("value.originalFileName", originalFileName);
-                                fieldWriter.AddField("value.fileSize", fileSize);
-                                fieldWriter.AddField("value.description", description);
-                                fieldWriter.AddField("value.uploadedByUser", uploadedByUser);
-                                fieldWriter.AddField("value.uploadedBy", uploadedBy);
-                                fieldWriter.AddField("value.uploadedOn", uploadedOn);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        fieldType = FieldType.Object;
-                        csvHeaders.Add($"{kvp.Key}.value");
-                        csvItems.Add(valueString);
-                    }
-                    JsonElement completed = nestedObjects.FirstOrDefault(x => x.Name == "completed").Value;
-                    csvHeaders.Add($"{kvp.Key}.completed");
-                    csvItems.Add(completed.ToString());
+                    FieldExporter fieldExporter = FieldExporterFactory.Create(jsonElement);
+                    fieldExporter.Export(kvp, csv);
                 }
                 else
                 {
-                    csvHeaders.Add(kvp.Key);
-                    csvItems.Add(kvp.Value?.ToString() ?? string.Empty);
+                    // TODO is this the right approach for non-object values? Should we just skip them or handle them differently?
+                    csv.AddItem(kvp.Key, kvp.Value?.ToString() ?? string.Empty);
                 }
             }
 
-            var csvHeader = string.Join(", ", csvHeaders);
-            var csvData = string.Join(", ", csvItems);
-            return $"{csvHeader}{Environment.NewLine}{csvData}";
+            return csv.Export();
         }
 
         public string? GenerateJson(string html)
@@ -149,6 +102,129 @@ namespace DfE.ExternalApplications.Web.Services
             templateData.Groups = groups;
 
             return JsonSerializer.Serialize(templateData, serializerOptions);
+        }
+
+        public static class FieldExporterFactory
+        {
+            private static readonly List<FieldExporter> exporters =
+            [
+                new ComplexFieldExporter(),
+                new FileUploadFieldExporter(),
+                new SimpleFieldExporter()
+            ];
+
+            public static FieldExporter Create(JsonElement jsonElement)
+            {
+                return exporters.FirstOrDefault(exporter => exporter.CanExport(jsonElement)) ?? throw new Exception("Unknown field type");
+            }
+        }
+
+        public abstract class FieldExporter
+        {
+            public abstract bool CanExport(JsonElement value);
+
+            public abstract void Export(dynamic kvp, Csv csv);
+        }
+
+        public class SimpleFieldExporter : FieldExporter
+        {
+            public override bool CanExport(JsonElement value) => true; // HACK: for now, assume any field that is not a complex or file upload field is a simple field
+
+            public override void Export(dynamic kvp, Csv csv)
+            {
+                if (kvp.Value is not JsonElement jsonElement || jsonElement.ValueKind != JsonValueKind.Object)
+                {
+                    return;
+                }
+
+                JsonElement.ObjectEnumerator nestedObjects = jsonElement.EnumerateObject();
+                JsonElement value = nestedObjects.FirstOrDefault(x => x.Name == "value").Value;
+                csv.AddItem($"{kvp.Key}.value", value.ToString());
+                JsonElement completed = nestedObjects.FirstOrDefault(x => x.Name == "completed").Value;
+                csv.AddItem($"{kvp.Key}.completed", completed.ToString());
+            }
+        }
+
+        public class ComplexFieldExporter : FieldExporter
+        {
+            public override bool CanExport(JsonElement value)
+            {
+                // TODO implement logic to determine if this exporter can handle the given JSON structure
+                return false;
+            }
+
+            public override void Export(dynamic kvp, Csv csv)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class FileUploadFieldExporter : FieldExporter
+        {
+            public override bool CanExport(JsonElement jsonElement)
+            {
+                JsonElement.ObjectEnumerator nestedObjects = jsonElement.EnumerateObject();
+                JsonElement value = nestedObjects.FirstOrDefault(x => x.Name == "value").Value;
+                var valueString = value.ToString();
+                if (valueString.StartsWith('[') && valueString.EndsWith(']') && valueString.Contains("fileName"))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public override void Export(dynamic kvp, Csv csv)
+            {
+                if (kvp.Value is not JsonElement jsonElement || jsonElement.ValueKind != JsonValueKind.Object)
+                {
+                    return;
+                }
+
+                JsonElement.ObjectEnumerator nestedObjects = jsonElement.EnumerateObject();
+                JsonElement value = nestedObjects.FirstOrDefault(x => x.Name == "value").Value;
+                var valueString = value.ToString();
+                IEnumerable<JsonElement> objects = JsonSerializer.Deserialize<IEnumerable<JsonElement>>(valueString)!;
+                Debug.WriteLine($"{objects.Count()} file upload objects");
+                foreach (JsonElement obj2 in objects)
+                {
+                    JsonElement.ObjectEnumerator nestedObjects2 = obj2.EnumerateObject();
+                    JsonElement originalFileName = nestedObjects2.FirstOrDefault(x => x.Name == "originalFileName").Value;
+                    JsonElement description = nestedObjects2.FirstOrDefault(x => x.Name == "description").Value;
+                    JsonElement fileSize = nestedObjects2.FirstOrDefault(x => x.Name == "fileSize").Value;
+                    JsonElement uploadedBy = nestedObjects2.FirstOrDefault(x => x.Name == "uploadedBy").Value;
+                    JsonElement uploadedByUser = nestedObjects2.FirstOrDefault(x => x.Name == "uploadedByUser").Value;
+                    JsonElement uploadedOn = nestedObjects2.FirstOrDefault(x => x.Name == "uploadedOn").Value;
+                    Debug.WriteLine($"{kvp.Key}.value - originalFileName: {originalFileName}, fileSize: {fileSize}.");
+                    var prefix = $"{kvp.Key}.value";
+                    csv.AddItem($"{prefix}.originalFileName", originalFileName.ToString());
+                    csv.AddItem($"{prefix}.description", description.ToString());
+                    csv.AddItem($"{prefix}.fileSize", fileSize.ToString());
+                    csv.AddItem($"{prefix}.uploadedBy", uploadedBy.ToString());
+                    csv.AddItem($"{prefix}.uploadedByUser", uploadedByUser.ToString());
+                    csv.AddItem($"{prefix}.uploadedOn", uploadedOn.ToString());
+                }
+            }
+        }
+
+        public class Csv
+        {
+            public List<string> Headers { get; internal set; } = [];
+            public List<string> Items { get; internal set; } = [];
+
+            internal void AddItem(string header, string item)
+            {
+                Headers.Add(header);
+                Items.Add(item);
+            }
+
+            public string Export()
+            {
+                if (Headers.Count != Items.Count) throw new InvalidOperationException("Headers and Items must have the same count.");
+                var csvHeader = string.Join(", ", Headers);
+                var csvData = string.Join(", ", Items);
+                return $"{csvHeader}{Environment.NewLine}{csvData}";
+            }
         }
     }
 
--- src/Tests/Dfe.ExternalApplications.Web.UnitTests/Services/ApplicationCsvGeneratorTest.cs
+++ src/Tests/Dfe.ExternalApplications.Web.UnitTests/Services/ApplicationCsvGeneratorTest.cs
@@ -1,5 +1,8 @@
 ﻿using DfE.ExternalApplications.Web.Services;
+using System.Dynamic;
+using System.Text.Json;
 using Xunit.Abstractions;
+using static DfE.ExternalApplications.Web.Services.ApplicationCsvGenerator;
 
 namespace Dfe.ExternalApplications.Web.UnitTests.Services
 {
@@ -48,6 +51,7 @@ namespace Dfe.ExternalApplications.Web.UnitTests.Services
         [Fact]
         public void Generate2()
         {
+            // TODO test collection field type: year-1-delivery-plan/year-1-workstreams?
             string json = File.ReadAllText(@"Services\application-data.json");
 
             string csv = generator.Generate2("app-ref-1", json);
@@ -56,6 +60,131 @@ namespace Dfe.ExternalApplications.Web.UnitTests.Services
             testOutput.WriteLine(csv);
 
             // TODO check csv fields
+        }
+
+        [Fact]
+        public void FieldExporterFactory_Create_Simple()
+        {
+            var fields = JsonSerializer.Deserialize<IDictionary<string, object>>(@"
+              {
+                  ""starting-year"": {
+                    ""value"": ""2027"",
+                    ""completed"": true
+                  }
+              }")!;
+            JsonElement field = (JsonElement)fields.FirstOrDefault().Value;
+
+            FieldExporter fieldExporter = FieldExporterFactory.Create(field);
+
+            Assert.NotNull(fieldExporter);
+            Assert.IsType<SimpleFieldExporter>(fieldExporter);
+        }
+
+        [Fact]
+        public void FieldExporterFactory_Create_FileUpload()
+        {
+            var fields = JsonSerializer.Deserialize<IDictionary<string, object>>(@"
+              {
+                ""upload-blueprint-supporting-evidence"": {
+                    ""value"": ""[{\""id\"":\""1f6475d5-7e3e-4f3b-b34c-a543ad49371c\"",\""applicationId\"":\""67708b10-a406-4f67-a5d9-2fa212e7c121\"",\""uploadedBy\"":\""00000000-0000-0000-0000-000000000001\"",\""uploadedByUser\"":null,\""name\"":\""Upload Test.png\"",\""description\"":\""FileDescription\"",\""originalFileName\"":\""Upload Test.png\"",\""fileName\"":\""8876cbe82e813060ee1532e03e5cd900.png\"",\""fileSize\"":7928,\""uploadedOn\"":\""2026-07-09T10:32:53.4880739\""}]"",
+                    ""completed"": true
+                }
+              }")!;
+            JsonElement field = (JsonElement)fields.FirstOrDefault().Value;
+
+            FieldExporter fieldExporter = FieldExporterFactory.Create(field);
+
+            Assert.NotNull(fieldExporter);
+            Assert.IsType<FileUploadFieldExporter>(fieldExporter);
+        }
+
+        [Fact]
+        public void SimpleFieldExporter()
+        {
+            var fields = JsonSerializer.Deserialize<ExpandoObject>(@"
+              {
+                  ""starting-year"": {
+                    ""value"": ""2027"",
+                    ""completed"": true
+                  }
+              }")!;
+
+            // HACK must be a better way to get the first field from an ExpandoObject
+            dynamic? field = default;
+            foreach (var kvp in fields)
+            {
+                field = kvp;
+                break;
+            }
+
+            var fieldValue = (JsonElement)field!.Value;
+            JsonElement.ObjectEnumerator nestedObjects = fieldValue.EnumerateObject();
+            JsonElement value = nestedObjects.FirstOrDefault(x => x.Name == "value").Value;
+
+            Csv csv = new();
+
+            SimpleFieldExporter exporter = new();
+            exporter.Export(field, csv);
+
+            testOutput.WriteLine(csv.Export());
+
+            Assert.Equal(2, csv.Headers.Count);
+            Assert.Equal(2, csv.Items.Count);
+            Assert.Equal("starting-year.value", csv.Headers.ElementAt(0));
+            Assert.Equal("2027", csv.Items.ElementAt(0));
+            Assert.Equal("starting-year.completed", csv.Headers.ElementAt(1));
+            Assert.Equal("True", csv.Items.ElementAt(1));
+        }
+
+        [Fact]
+        public void FileUploadFieldExporter()
+        {
+            var fields = JsonSerializer.Deserialize<ExpandoObject>(@"
+              {
+                ""upload-blueprint-supporting-evidence"": {
+                    ""value"": ""[{\""id\"":\""1f6475d5-7e3e-4f3b-b34c-a543ad49371c\"",\""applicationId\"":\""67708b10-a406-4f67-a5d9-2fa212e7c121\"",\""uploadedBy\"":\""00000000-0000-0000-0000-000000000001\"",\""uploadedByUser\"":null,\""name\"":\""Upload Test.png\"",\""description\"":\""FileDescription\"",\""originalFileName\"":\""Upload Test.png\"",\""fileName\"":\""8876cbe82e813060ee1532e03e5cd900.png\"",\""fileSize\"":7928,\""uploadedOn\"":\""2026-07-09T10:32:53.4880739\""}]"",
+                    ""completed"": true
+                }
+              }")!;
+
+            // HACK must be a better way to get the first field from an ExpandoObject
+            dynamic? field = default;
+            foreach (var kvp in fields)
+            {
+                field = kvp;
+                break;
+            }
+
+            var fieldValue = (JsonElement)field!.Value;
+            JsonElement.ObjectEnumerator nestedObjects = fieldValue.EnumerateObject();
+            JsonElement value = nestedObjects.FirstOrDefault(x => x.Name == "value").Value;
+
+            Csv csv = new();
+
+            FileUploadFieldExporter exporter = new();
+            exporter.Export(field, csv);
+
+            testOutput.WriteLine($"CSV has {csv.Headers.Count} fields");
+            testOutput.WriteLine(csv.Export());
+
+            Assert.Equal(6, csv.Headers.Count);
+            CsvChecker checker = new(csv, "upload-blueprint-supporting-evidence.value");
+            short index = 0;
+            checker.CheckField(index++, "originalFileName", "Upload Test.png");
+            checker.CheckField(index++, "description", "FileDescription");
+            checker.CheckField(index++, "fileSize", "7928");
+            checker.CheckField(index++, "uploadedBy", "00000000-0000-0000-0000-000000000001");
+            checker.CheckField(index++, "uploadedByUser", "");
+            checker.CheckField(index++, "uploadedOn", "2026-07-09T10:32:53.4880739");
+        }
+    }
+
+    internal class CsvChecker(Csv csv, string prefix)
+    {
+        internal void CheckField(short index, string header, string item)
+        {
+            Assert.Equal($"{prefix}.{header}", csv.Headers.ElementAt(index));
+            Assert.Equal(item, csv.Items.ElementAt(index));
         }
     }
 }
